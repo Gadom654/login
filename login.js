d31842a5d64dd41c3878bbd305e72ba3a9d5d778
Gadom <69717399+Gadom654@users.noreply.github.com>
2022-11-26 11:44:32 +0100

    docs

--- login.js
+++ login.js
@@ -1,12 +1,15 @@
+//creating some variables to use later
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
+    //function to get user inputs and validate it
+    //creating vars of user input
     const username = loginForm.username.value;
     const password = loginForm.password.value;
-
+    //checking if username and password is good print logged in info if not print alert about wrong login
     if (username === "lol" && password === "lol") {
         alert("You have successfully logged in.");
         location.reload();
